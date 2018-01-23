a93a11c506a20bf5b1d01a54d254f31798e1220e
sky-adams <skyadams@sbunified.org>
2018-01-23 08:38:00 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,12 +7,10 @@ app = Flask(__name__)
 
 # In order to use "sessions",you need a "secret key".
 # This is something random you generate.  
-# Just type some arbitrary letters and numbers.
 # See: http://flask.pocoo.org/docs/0.10/quickstart/#sessions
 
 app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
                                      #The value should be set in Heroku (Settings->Config Vars).  
-                                     #To run locally, set in env.sh and include that file in gitignore so the secret key is not made public.
 
 @app.route('/')
 def renderMain():
@@ -20,7 +18,7 @@ def renderMain():
 
 @app.route('/startOver')
 def startOver():
-    session.clear() #clears variable values and creates a new session
+    #clear variable values and create a new session
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
 @app.route('/page1')
@@ -29,14 +27,13 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    session["firstName"]=request.form['firstName']
-    session["lastName"]=request.form['lastName']
+    #set the first and last name in the session
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["favoriteColor"]=request.form['favoriteColor']
+    #set the favorite color in the session
     return render_template('page3.html')
     
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=54321)
+    app.run(debug=False)

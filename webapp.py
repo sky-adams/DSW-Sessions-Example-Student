9f1840e5425962866293e7a36faf68b08cfad19a
sky-adams <skyadams@sbunified.org>
2019-01-22 11:02:48 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,7 +19,7 @@ def renderMain():
 @app.route('/startOver')
 def startOver():
     #TODO: delete everything from the session
-    
+    session.clear()
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
 @app.route('/page1')
@@ -29,13 +29,14 @@ def renderPage1():
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
     #TODO: set the first and last name in the session
-    
+    session["firstName"] = request.form["firstName"] #adds the first name to the cookie
+    session["lastName"] = request.form["lastName"]
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
     #TODO: set the favorite color in the session
-    
+    session["favoriteColor"] = request.form["favoriteColor"]
     return render_template('page3.html')
     
 if __name__=="__main__":
